2c40450bb5afc1cdbaa15824f824fbe3e7517eb1
akashmondaldev <133047085+akashmondaldev@users.noreply.github.com>
2023-05-20 01:02:18 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,14 +4,14 @@ const host = process.env.HOST
 const user = process.env.USER
 const password = process.env.PASSWORD
 const database = process.env.DATABASE
-// const port = process.env.MYSQLPORT 
+const port = process.env.MYSQLPORT 
 
 const connect_mysql = mysql.createPool({
     host: host,
     user: user,
     password: password,
     database: database,
-    // port: port,
+    port: port,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
