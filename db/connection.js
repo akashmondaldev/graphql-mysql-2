6e9c092212c1609fa264267cdb3fe94cd06e23b7
akashmondaldev <133047085+akashmondaldev@users.noreply.github.com>
2023-05-20 18:16:18 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 require('dotenv').config()
-const host = process.env.HOST
-const user = process.env.USER
-const password = process.env.PASSWORD
-const database = process.env.DATABASE
-const port = process.env.MYSQLPORT 
+const host = "containers-us-west-113.railway.app"
+const user = "root"
+const password = "95z2JFyUBHaBrA3EkHjX"
+const database = "railway"
+const port = "7717"
 
 const connect_mysql = mysql.createPool({
     host: host,
