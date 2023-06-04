764e82641177a18e99d0bb54a4e399cb80cc6f88
akashmondaldev <133047085+akashmondaldev@users.noreply.github.com>
2023-06-04 13:15:45 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql2')
 require('dotenv').config()
-const host = "containers-us-west-113.railway.app"
-const user = "root"
-const password = "95z2JFyUBHaBrA3EkHjX"
-const database = "railway"
-const port = "7717"
+const host = "skyinc.mysql.database.azure.com"
+const user = "akash"
+const password = "olivia@2003"
+const database = "msdb"
+// const port = "7717"
 
 const connect_mysql = mysql.createPool({
     host: host,
     user: user,
     password: password,
     database: database,
-    port: port,
+//     port: port,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
